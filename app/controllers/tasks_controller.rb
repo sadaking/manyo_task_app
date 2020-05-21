92a293310380252166ff761329cbc2f31c9560d5
sadaking <sichirigahama.37@gmail.com>
2020-05-21 14:12:29 +0900

    タスク作成機能の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,27 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
     @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to new_task_path, notice:"タスクを作成しました！"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
