ea620921d44933cef00137405c46c348abca2ec8
sadaking <sichirigahama.37@gmail.com>
2020-05-22 15:37:21 +0900

    作成日時の降順での並び替えのテスト

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,11 +1,31 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  before do
+    # 「タスク一覧画面」や「タスク詳細画面」などそれぞれのテストケースで、before内のコードが実行される
+    # 各テストで使用するタスクを1件作成する
+    # 作成したタスクオブジェクトを各テストケースで呼び出せるようにインスタンス変数に代入
+    @task = FactoryBot.create(:task, title: 'task')
+    @second_task = FactoryBot.create(:task, title: 'new_task')
+  end
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示される' do
-        task = FactoryBot.create(:task, title: 'task')
+        # beforeに必要なタスクデータが作成されるので、ここでテストデータ作成処理を書く必要がない
         visit tasks_path
         expect(page).to have_content 'task'
+        expect(page).to have_content 'new_task'
+      end
+    end
+    context '複数のタスクを作成した場合' do
+      it 'タスクが作成日時の降順に並んでいる' do
+        # あらかじめタスク並び替えの確認テストで使用するためのタスクを二つ作成する
+        # （上記と全く同じ記述が繰り返されてしまう！）
+        visit tasks_path
+        
+        task_list = all('.task_row') # タスク一覧を配列として取得するため、View側でidを振っておく
+        
+        expect(task_list[0]).to have_content 'new_task'
+        expect(task_list[1]).to have_content 'task'
       end
     end
   end
