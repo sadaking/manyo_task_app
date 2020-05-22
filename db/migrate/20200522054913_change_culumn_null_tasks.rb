65a35ef20131b257270aa1cecc847a28a900ebce
sadaking <sichirigahama.37@gmail.com>
2020-05-22 15:30:23 +0900

    DBの制約設定(中身記載)

--- db/migrate/20200522054913_change_culumn_null_tasks.rb
+++ db/migrate/20200522054913_change_culumn_null_tasks.rb
@@ -1,6 +1,6 @@
 class ChangeCulumnNullTasks < ActiveRecord::Migration[5.2]
   def change
-    change_column :tasks, :name, :string, null: false
-    change_column :tasks, :detail, :text, null: false
+    change_column :tasks, :title, :string, null: false
+    change_column :tasks, :content, :text, null: false
   end
 end
