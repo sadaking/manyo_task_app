860f41302f2f6209e9a8bad9baa5618e81d9b8df
sadaking <sichirigahama.37@gmail.com>
2020-05-25 11:57:40 +0900

    タイムゾーンの修正

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module TaskApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 5.2
     config.generators do |g|
       g.test_framework :rspec,
