f3a9f665ecffe3f40d23dd01bb14932ccb488a09
sadaking <sichirigahama.37@gmail.com>
2020-05-26 18:12:56 +0900

    バリデーションの追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :deadline, presence: true
+  validates :status, presence: true
 
   scope :title_search, -> (title) {where("title LIKE ?", "%#{title}%")}
   scope :status_search,  -> (status) {where(status: status)}
