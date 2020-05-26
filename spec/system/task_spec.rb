f612a9d3bfbda8aa9709fc4338a2ed370c6eb2f4
sadaking <sichirigahama.37@gmail.com>
2020-05-26 18:15:26 +0900

    statusテストの作成

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+
   before do
-    # 「タスク一覧画面」や「タスク詳細画面」などそれぞれのテストケースで、before内のコードが実行される
-    # 各テストで使用するタスクを1件作成する
-    # 作成したタスクオブジェクトを各テストケースで呼び出せるようにインスタンス変数に代入
-    @task = FactoryBot.create(:task, title: 'task', content: 'submit task', deadline: '2020-05-23 00:00:00')
-    @second_task = FactoryBot.create(:task, title: 'new_task',content: 'difficult task', deadline: '2020-05-24 00:00:00')
+    @task = FactoryBot.create(:task, title: 'task', content: 'submit task', deadline: '2020-05-23 00:00:00', status: '完了')
+    @second_task = FactoryBot.create(:task, title: 'new_task',content: 'difficult task', deadline: '2020-05-24 00:00:00', status: '着手中')
   end
+
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示される' do
-        # beforeに必要なタスクデータが作成されるので、ここでテストデータ作成処理を書く必要がない
         visit tasks_path
         expect(page).to have_content 'task'
         expect(page).to have_content 'new_task'
@@ -18,17 +16,36 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context '複数のタスクを作成した場合' do
       it 'タスクが作成日時の降順に並んでいる' do
-        # あらかじめタスク並び替えの確認テストで使用するためのタスクを二つ作成する
-        # （上記と全く同じ記述が繰り返されてしまう！）
         visit tasks_path
-
-        task_list = all('.task_row') # タスク一覧を配列として取得するため、View側でidを振っておく
-
+        task_list = all('.task_row')
         expect(task_list[0]).to have_content 'new_task'
         expect(task_list[1]).to have_content 'task'
       end
     end
+    context 'scopeメソッドで検索をした場合' do
+      it "scopeメソッドでタイトル検索ができる" do
+        visit tasks_path
+        fill_in 'title', with: 'new_task'
+        click_on '検索する'
+        expect(page).to have_content 'new_task'
+      end
+      it "scopeメソッドでステータス検索ができる" do
+        visit tasks_path
+        select '着手中', from: "status"
+        click_on '検索する'
+        expect(page).to have_content '着手中'
+      end
+      it "scopeメソッドでタイトルとステータスの両方が検索できる" do
+        visit tasks_path
+        fill_in 'title', with: 'new_task'
+        select '着手中', from: "status"
+        click_on '検索する'
+        expect(page).to have_content 'new_task'
+        expect(page).to have_content '着手中'
+      end
+    end
   end
+
   describe 'タスク登録画面' do
     context '必要項目を入力して、createボタンを押した場合' do
       it 'データが保存される' do
@@ -41,6 +58,7 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
+
   describe 'タスク詳細画面' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示されたページに遷移する' do
