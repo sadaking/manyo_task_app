b175b8fee14a702ad5841c69409a66ecbd96f7d1
sadaking <sichirigahama.37@gmail.com>
2020-05-26 13:01:36 +0900

    null制約の追加

--- db/migrate/20200522054913_change_culumn_null_tasks.rb
+++ db/migrate/20200522054913_change_culumn_null_tasks.rb
@@ -2,5 +2,6 @@ class ChangeCulumnNullTasks < ActiveRecord::Migration[5.2]
   def change
     change_column :tasks, :title, :string, null: false
     change_column :tasks, :content, :text, null: false
+    change_column :tasks, :deadline, :datetime, null: false
   end
 end
