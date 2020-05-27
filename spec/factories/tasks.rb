68eec4d60b5cb00b38b40d6d047de5eb5e96f716
sadaking <sichirigahama.37@gmail.com>
2020-05-27 23:16:08 +0900

    各機能のModel Specを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,14 +1,16 @@
 FactoryBot.define do
-  # 作成するテストデータの名前を「task」とします
-  # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
   factory :task do
-    title { 'Factoryで作ったデフォルトのタイトル１' }
-    content { 'Factoryで作ったデフォルトのコンテント１' }
+    title { '万葉' }
+    content { '万葉課題' }
+    deadline { '2020-05-27' }
+    status { '完了' }
+    priority { '高' }
   end
-  # 作成するテストデータの名前を「second_task」とします
-  # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
   factory :second_task, class: Task do
-    title { 'Factoryで作ったデフォルトのタイトル２' }
-    content { 'Factoryで作ったデフォルトのコンテント２' }
+    title { 'twitter' }
+    content { 'twitter作成' }
+    deadline { '2020-05-28' }
+    status { '完了' }
+    priority { '中' }
   end
 end
