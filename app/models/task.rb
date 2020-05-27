a7be3621a14fdb03616ca1a340144073d88f9014
sadaking <sichirigahama.37@gmail.com>
2020-05-27 12:01:29 +0900

    priorityのバリデーション設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,9 @@ class Task < ApplicationRecord
   validates :content, presence: true
   validates :deadline, presence: true
   validates :status, presence: true
+  validates :priority, presence: true
+
+  enum priority: {高:0, 中:1, 低:2}
 
   scope :title_search, -> (title) {where("title LIKE ?", "%#{title}%")}
   scope :status_search,  -> (status) {where(status: status)}
