242de09832fd13acec0be45ddeb13cd567cfc53c
sadaking <sichirigahama.37@gmail.com>
2020-06-02 20:44:30 +0900

    ユーザ詳細画面でtask一覧を観れるように修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -56,7 +56,6 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation, :admin)
