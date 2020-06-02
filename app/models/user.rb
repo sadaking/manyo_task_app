f245b1af0bb70d9c65c41dca73a38297613794aa
sadaking <sichirigahama.37@gmail.com>
2020-06-02 14:13:54 +0900

    モデルでのコールバックを使用して条件分岐

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,24 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
+
+  before_update :admin_update_restriction?
+  before_destroy :admin_destroy_restriction?
+
+  private
+
+  def admin_update_restriction?
+    @admin_user = User.where(admin: true)
+    if (@admin_user.count == 1 && @admin_user.first == self) && !(self.admin?)
+      throw :abort
+    end
+  end
+
+
+  def admin_destroy_restriction?
+    if User.where(admin: true).count <=1 && self.admin == true
+      throw :abort
+    end
+  end
 end
